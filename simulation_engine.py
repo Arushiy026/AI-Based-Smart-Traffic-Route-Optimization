1454472a57c759efb3bb06458b9a8ff3d6b3d278
Arushiy026 <24csu026@ncuindia.edu>
2026-05-28 10:42:32 +0530

    Add files via upload

---
+++ simulation_engine.py
@@ -0,0 +1,92 @@
+[
+    {
+        "source": "rajiv_chowk",
+        "target": "jantar_mantar",
+        "distance": 1.2,
+        "traffic": 5
+    },
+    {
+        "source": "jantar_mantar",
+        "target": "rajiv_chowk",
+        "distance": 1.2,
+        "traffic": 4
+    },
+    {
+        "source": "rajiv_chowk",
+        "target": "barakhamba",
+        "distance": 0.8,
+        "traffic": 6
+    },
+    {
+        "source": "barakhamba",
+        "target": "rajiv_chowk",
+        "distance": 0.8,
+        "traffic": 5
+    },
+    {
+        "source": "barakhamba",
+        "target": "mandi_house",
+        "distance": 1.5,
+        "traffic": 3
+    },
+    {
+        "source": "mandi_house",
+        "target": "barakhamba",
+        "distance": 1.5,
+        "traffic": 4
+    },
+    {
+        "source": "rajiv_chowk",
+        "target": "new_delhi_rs",
+        "distance": 1.5,
+        "traffic": 8
+    },
+    {
+        "source": "new_delhi_rs",
+        "target": "paharganj",
+        "distance": 0.9,
+        "traffic": 7
+    },
+    {
+        "source": "paharganj",
+        "target": "jantar_mantar",
+        "distance": 2.2,
+        "traffic": 6
+    },
+    {
+        "source": "mandi_house",
+        "target": "ito",
+        "distance": 1.1,
+        "traffic": 9
+    },
+    {
+        "source": "ito",
+        "target": "supreme_court",
+        "distance": 1.2,
+        "traffic": 5
+    },
+    {
+        "source": "supreme_court",
+        "target": "mandi_house",
+        "distance": 1.0,
+        "traffic": 3
+    },
+    {
+        "source": "mandi_house",
+        "target": "india_gate",
+        "distance": 2.0,
+        "traffic": 7
+    },
+    {
+        "source": "india_gate",
+        "target": "khan_market",
+        "distance": 2.0,
+        "traffic": 4
+    },
+    {
+        "source": "jantar_mantar",
+        "target": "india_gate",
+        "distance": 2.5,
+        "traffic": 8
+    }
+]
